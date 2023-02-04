12fb27f61c552f1a7d11ad6507c63b6b0426f233
ApoioBCC IME-USP <80135166+apoiobcc-imeusp@users.noreply.github.com>
2023-02-04 16:49:06 -0400

    Atualiza o script de deploy sem variáveis do Travis

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-echo 'Deploying commit on BCC server...'
+echo 'Deploying to BCC server...'
 
-# only actualy deploy if is not pull request
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo 'Passo ignorado: Pull Request'
-	exit 0
+# only deploy if is a push event
+if [ "$GITHUB_EVENT_NAME" != "push" ]; then
+    echo 'Step ignored: Not a push event'
+    exit 0
 fi
 
-sshpass -p $PASSWORD_BCC ssh -o StrictHostKeyChecking=no $USERNAME_BCC@bcc.ime.usp.br PASSWORD_BCC=$PASSWORD_BCC bash --login -c 'bash -s' <<'ENDSSH'
+sshpass -p ${PASSWORD_BCC} ssh -o StrictHostKeyChecking=no ${USERNAME_BCC}@bcc.ime.usp.br "${PASSWORD_BCC}=${PASSWORD_BCC} bash --login -c 'bash -s'" <<'ENDSSH'
   # commands to run on remote host
-  echo $PASSWORD_BCC | sudo -S -p '' echo "Connected!" && \
+  echo ${PASSWORD_BCC} | sudo -S -p '' echo "Connected!" && \
   cd /home/felipen/apoiobcc && \
   pwd && \
   echo 'Updating repository' && \
   git pull && \
   echo 'Building' && \
   export rvmsudo_secure_path=0 && \
-  echo $PASSWORD_BCC | rvmsudo -S -p '' bundle exec jekyll build && \
+  echo ${PASSWORD_BCC} | rvmsudo -S -p '' bundle exec jekyll build && \
   echo 'SUCCESS!'
 ENDSSH
